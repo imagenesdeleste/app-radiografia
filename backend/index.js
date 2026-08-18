e59ee3e5118751bd680da2c173540771eeb2b64a
Axell Peraza <axellperaza@gmail.com>
2026-08-18 12:30:33 -0400

    corrección de direccion

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://fronted-production-a731.up.railway.app',
+  origin: 'https://www.unidaddeimagenesdeleste.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
