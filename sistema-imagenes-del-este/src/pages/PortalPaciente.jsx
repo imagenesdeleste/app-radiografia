88be02e0e6e995d3a228e09443ce575f99f1de66
Axell Peraza <axellperaza@gmail.com>
2026-08-25 08:43:17 -0400

    Boton mostrar clave

--- sistema-imagenes-del-este/src/pages/PortalPaciente.jsx
+++ sistema-imagenes-del-este/src/pages/PortalPaciente.jsx
@@ -69,7 +69,7 @@ export default function PortalPaciente() {
             className="w-full h-full object-contain drop-shadow-sm" 
           />
         </div>
-        <p className="text-xs text-slate-400 uppercase tracking-widest mt-0.5">Bienvenidos al portal de Unidad De Imagenes Del Este</p>
+        <p className="text-xs text-slate-400 uppercase tracking-widest mt-0.5">Bienvenido Estimado Paciente</p>
       </header>
 
       {/* CONTENIDO PRINCIPAL */}
